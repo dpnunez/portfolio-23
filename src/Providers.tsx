a86c43d411e3f1e71a46173742941403185dc107
Daniel Pôrto Núñez <dpnunez@Daniels-MacBook-Pro.local>
2023-09-19 13:00:52 -0300

    fix: remove theme workaround

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,19 +1,8 @@
 'use client'
 import { ThemeProvider } from 'next-themes'
-import { useState, useEffect } from 'react'
 import { Toaster } from 'react-hot-toast'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = useState(false)
-
-  useEffect(() => {
-    setMounted(true)
-  }, [])
-
-  if (!mounted) {
-    return <>{children}</>
-  }
-
   return (
     <ThemeProvider attribute="class" defaultTheme="dark">
       <Toaster
