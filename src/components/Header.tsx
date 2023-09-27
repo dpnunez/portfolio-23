b85e932c59a5ba084114f353383a26ad31d3b669
Daniel Pôrto Núñez <dpnunez@Daniels-MacBook-Pro.local>
2023-09-27 13:23:13 -0300

    feat: add resume link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,24 +19,39 @@ export function Header() {
   return (
     <header className="container h-24 flex items-center justify-between">
       <Logo />
-      <motion.button
-        aria-label="Toggle theme"
-        onClick={toggleTheme}
-        type="button"
-        animate={{
-          opacity: 1,
-        }}
-        transition={{
-          delay: 3,
-        }}
-        className="bg-primary-low-opacity rounded-full aspect-square p-4 opacity-0 flex justify-center items-center hover:brightness-125 transition-all duration-300"
-      >
-        {theme === 'dark' ? (
-          <MoonIcon width={16} height={16} />
-        ) : (
-          <SunIcon width={16} height={16} />
-        )}
-      </motion.button>
+      <div className="flex gap-6 items-center">
+        <motion.button
+          aria-label="Toggle theme"
+          onClick={toggleTheme}
+          type="button"
+          animate={{
+            opacity: 1,
+          }}
+          transition={{
+            delay: 3,
+          }}
+          className="bg-primary-low-opacity rounded-full aspect-square p-4 opacity-0 flex justify-center items-center hover:brightness-125 transition-all duration-300"
+        >
+          {theme === 'dark' ? (
+            <MoonIcon width={16} height={16} />
+          ) : (
+            <SunIcon width={16} height={16} />
+          )}
+        </motion.button>
+        <motion.a
+          href="/resume.pdf"
+          target="_blank"
+          animate={{
+            opacity: 1,
+          }}
+          transition={{
+            delay: 3,
+          }}
+          className=" rounded-lg p-2 opacity-0 flex justify-center items-center hover:brightness-125 transition-all duration-300 ring-1 ring-primary-dark ring-opacity-40"
+        >
+          Resume
+        </motion.a>
+      </div>
     </header>
   )
 }
